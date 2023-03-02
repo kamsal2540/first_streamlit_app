c54afd4e61e3978191aeb6929d216fd0ec04803f
kamsal2540 <53993533+kamsal2540@users.noreply.github.com>
2023-03-02 17:28:28 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title("Kamal Saluja")
+streamlit.title("Kamal Saluja Restaurant")
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Rang Egg")
+streamlit.text("🥑🍞 Avocado Toast")
